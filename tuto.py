075c9b9765cff8e1590b657d66af224540faff48
TCHOING Raoul <tchoing50@gmail.com>
2022-09-06 21:10:54 +0200

    POO CORRECTION EXERCICE DE MISE EN SITUATION 2

--- tuto.py
+++ tuto.py
@@ -926,3 +926,58 @@ personne1.SePresenter()
 personne2 = Personne("Emilie", 17, False)
 personne2.SePresenter()'''
 
+# POO EXERCICE DE MISE EN SITUATION 2
+
+'''class Personne:
+    def __init__(self, nom: str):
+        self.nom = nom   # crée une variable d'instance : nom
+        print("Constructeur personne " + self.nom)
+
+    def SePresenter(self, age: int):
+        # Bonjour, je m'appelle Jean, j'ai 30 ans
+        # Je suis majeur
+        print("Bonjour, je m'appelle " + self.nom + ", j'ai " + str(age) + " ans")
+        if self.EstMajeur():
+            print("Je suis majeur")
+        else:
+            print("Je suis mineur")
+        print()
+
+    def EstMajeur(self):
+        return age >= 18
+
+personne1 = Personne("Jean")
+personne1.SePresenter(25)
+
+personne1 = Personne("Emilie")
+personne1.SePresenter(17)'''
+
+# POO CORRECTION EXERCICE DE MISE EN SITUATION 2
+
+'''class Personne:
+    def __init__(self, nom: str, age: int):
+        self.nom = nom   # crée une variable d'instance : nom
+        self.age = age
+        print("Constructeur personne " + self.nom)
+
+    def SePresenter(self):
+        # Bonjour, je m'appelle Jean, j'ai 30 ans
+        # Je suis majeur
+        print("Bonjour, je m'appelle " + self.nom + ", j'ai " + str(self.age) + " ans")
+        
+        
+        if self.EstMajeur():
+            print("Je suis majeur")
+        else:
+            print("Je suis mineur")
+        print()
+
+    def EstMajeur(self):
+        return self.age >= 18
+
+personne1 = Personne("Jean", 25)
+personne1.SePresenter()
+
+personne2 = Personne("Emilie", 17)
+personne2.SePresenter()'''
+
